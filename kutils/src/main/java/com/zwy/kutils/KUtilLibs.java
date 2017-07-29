4b0d219a62d7205d979a23ac922000a67f417f10
dev_zwy@aliyun.com <ai930225>
2017-07-29 09:53:03 +0800

    修改了Appt的检测为false,在init方法中增加V2.1测试日志输出。将提交更新jcenter仓库！

--- kutils/src/main/java/com/zwy/kutils/KUtilLibs.java
+++ kutils/src/main/java/com/zwy/kutils/KUtilLibs.java
@@ -10,12 +10,12 @@ import com.zwy.kutils.widget.loadingdialog.DialogUIUtils;
  * ================================================================
  * 创建时间：2017/7/27 上午9:27
  * 创建人：Alan
- * 文件描述：
+ * 文件描述：基类
  * 至尊宝：长夜漫漫无心睡眠，我以为只有我睡不着，原来晶晶姑娘你也睡不着 ！
  * ================================================================
  */
 public class KUtilLibs {
-
+    private static String TAG ="KUtilLibs";
     /**
      * 初始化库
      *
@@ -24,6 +24,7 @@ public class KUtilLibs {
      * @param context application
      */
     public static void init(@NonNull boolean isDebug, @NonNull String TAG, @NonNull Context context) {
+        android.util.Log.d(TAG,"看到这条消息表示KUtilsV2.1版本更新");
         if (TAG == null || context == null) throw new RuntimeException("KUtilLibs 初始化参数均不能为空");
         if (isDebug) Log.init(TAG, true);//开启日志打印
         DialogUIUtils.init(context);
